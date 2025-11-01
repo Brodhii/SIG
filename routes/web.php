03c7850b9197f2d1e4d6c12cc9d041ed6aae9fa1
Brodhii <gustiaryaazharrodhi@gmail.com>
2025-11-01 21:16:28 +0700

    ubah tampilan

--- routes/web.php
+++ routes/web.php
@@ -3,20 +3,16 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CuacaController;
 
+Route::view('/kontak', 'kontak')->name('kontak');
+Route::view('/peta', 'peta')->name('peta');
+
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::view('/peta', 'peta')->name('peta');
-Route::view('/kontak', 'kontak')->name('kontak');
+Route::get('/cuaca', [CuacaController::class, 'index']);
 Route::get('/cuaca', [CuacaController::class, 'index'])->name('cuaca');
-<<<<<<< HEAD
-Route::get('/', [CuacaController::class, 'home'])->name('home');
-=======
 Route::get('/', [CuacaController::class, 'home'])->name('home');
 
 
 
-
-
->>>>>>> ead4742 (ubah tampilan)
