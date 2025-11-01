4619e6383937a23c2dafa1d8b0c0d26077ee9f81
Brodhii <gustiaryaazharrodhi@gmail.com>
2025-11-01 21:56:48 +0700

    ok

--- routes/web.php
+++ routes/web.php
@@ -3,15 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CuacaController;
 
+Route::get('/', [CuacaController::class, 'home'])->name('home');
+Route::get('/cuaca', [CuacaController::class, 'index'])->name('cuaca');
 Route::view('/kontak', 'kontak')->name('kontak');
 Route::view('/peta', 'peta')->name('peta');
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/cuaca', [CuacaController::class, 'index'])->name('cuaca');
-Route::get('/', [CuacaController::class, 'home'])->name('home');
-
-
-
